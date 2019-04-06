1c36c8ddbce7757ea3f7d9a246a9671b367c9804
Adeosun Abiola <abiolaolalekan39@gmail.com>
2019-04-06 06:51:43 +0100

    Added new profile

---
+++ views/interns/abitech105.php
@@ -0,0 +1,37 @@
+<html>
+   <?php include 'partials/header.php';?>
+   <body class=" pi">
+      <?php include 'partials/navbar.php';?>
+      <section class="main-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 col-sm-6 col-xs-12">
+                  <div class="col-md-6 col-xs-12">
+                     <div class="intern-block border-blue">
+                        <img src="https://res.cloudinary.com/dlxlb7wtj/image/upload/v1554528871/abiosky.jpg" alt="Adeosun Abiola">
+                        <h6>Adeosun Abiola</h6>
+                        <p>Front-end Engineer</p>
+                        <ul class="list">
+                           <li><a href="https://twitter.com/abiola_adeosun" target="_blank"<i class="fab fa-twitter fa-2x"></i></a></li>
+                           <li><a href="https://github.com/abitechcoder" target="_blank"<i class="fab fa-github fa-2x"></i></a></li>
+                           <li><a href="https://medium.com/@Abitechcoder" target="_blank"<i class="fab fa-medium fa-2x"></i></a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-xs-12 align-items-center">
+                     <div class="intern-bio border-blue">
+                        <h6>About me</h6>
+                        <p>My name is Adeosun Abiola Olalekan. My name is Adeosun Abiola Olalekan. I am currently studying computer science at Moshood Abiola Polytechnic, Ojere, Abeokuta. I am passionate about Technology and also love every avenue that makes me learn and improve.</p>
+                        <br />
+                        <h6>Skills</h6>
+                        <p>Java | PHP | JavaScript</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         </div>
+      </section>
+   </body>
+   <?php include 'partials/footer.php';?>
+</html>
